dad456383e5487818b625317b7a8c8d91fb152be
John David Rodríguez David <jrodriguez@cidenet.com.co>
2024-04-22 19:22:21 -0500

    final

--- Punto_3.py
+++ Punto_3.py
@@ -2,31 +2,48 @@ import requests
 
 URL = "https://my-json-server.typicode.com/JohnDrD-UdA/Trabajo_PI_1"
 
-print("##### get #####")
-request= requests.get(URL+"/users")
 
-print(request.json())
+try:
+    print("##### get #####")
+    request= requests.get(URL+"/users")
+    print(request.json())
+except:
+    print("Oh oh algo salio mal: get")
 
-print("##### post #####")
-request= requests.post(URL+"/users",{
-    "id": 4,
-    "name":"paco"
-})
-print(request.json())
 
 # De la documentacion de My-json-serve: 
 #resource will not be really updated on the server but it will be faked as if.
 #Se aplica para cada request que cambie el recurso, no se ve el cambio reflejado
+try:
+    print("##### post #####")
+    request= requests.post(URL+"/users",{
+    "id": 4,
+    "name":"paco"
+})
+    print(request.json())
+except:
+    print("Oh oh algo salio mal: post")
 
-print("##### put #####")
-request= requests.put(URL+"/users/1",{
+
+try:
+    print("##### put #####")
+    request= requests.put(URL+"/users/1",{
     "id": 1,
     "name":"julio"
-})
-print(request.json())
+    })
+    print(request.json())
+except:
+    print("Oh oh algo salio mal: put")
 
-print("##### delete #####")
-request= requests.delete(URL+"/users/1")
-print(request.json())
+
+try:
+    print("##### delete #####")
+    request= requests.delete(URL+"/users/1")
+    print(request.json())
+except:
+    print("Oh oh algo salio mal: delete")
+
+
+
 
 
